7f7aecff8b4de0deb0b8d79a14795e4e73f62a37
pogorelov-evgen <72943262+pogorelov-evgen@users.noreply.github.com>
2023-09-29 15:11:27 +0300

    Initial commit

---
+++ src/main/java/com/example/firststringboot/FirstStringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.firststringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FirstStringBootApplication.class, args);
+    }
+
+}
